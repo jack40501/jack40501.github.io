826c702966296e1976ef9a19eafc939dccceac2e
jack40501 <56793641+jack40501@users.noreply.github.com>
2019-11-04 19:28:37 +0800

    Add files via upload

--- js/top.js
+++ js/top.js
@@ -1,16 +1,53 @@
-// JavaScript Document
+  $(function(){
+ //control display of goTop button and motion
+    $("#gotop").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:0
+        },1000);
+    });
 
-$(function(){
-	
-	$("#goTop").click(function(){
-
-		$("html,body").animate({scrollTop:10},500);
-
-		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
-
-		return false;
-
-	});
-
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 150){
+            $('#gotop').fadeIn("fast");
+        } else {
+            $('#gotop').stop().fadeOut("fast");
+        }
+    });
 });
 
+$(window).scroll(function() {
+        if ( $(this).scrollTop() > 655){
+            $('#nav').fadeIn("fast");
+        } else {
+            $('#nav').stop().fadeOut("fast");
+        }
+    });
+;
+  $(function(){
+ //control display of goTop button and motion
+    $("#go1").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:750
+        },1000);
+    });},)
+   $(function(){
+ //control display of goTop button and motion
+    $("#go2").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:1400
+        },1000);
+    });},)
+    $(function(){
+ //control display of goTop button and motion
+    $("#go3").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:2850
+        },1000);
+    });},)
+     $(function(){
+ //control display of goTop button and motion
+    $("#go4").click(function(){
+        jQuery("html,body").animate({
+            scrollTop:4500
+        },1000);
+    });},)
