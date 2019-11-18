55f1b710766b08e6426c5a42d604d769bd2c80b2
jack40501 <56793641+jack40501@users.noreply.github.com>
2019-11-18 20:32:55 +0800

    Add files via upload

---
+++ buttonopen.js
@@ -0,0 +1,110 @@
+$(function() {
+
+            $("#彈跳框1").dialog({
+
+               autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a1").click(function() {
+
+                $("#彈跳框1").dialog("open");
+
+            });
+
+          
+
+        });
+     $(function() {
+
+            $("#彈跳框2").dialog({
+
+                autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a2").click(function() {
+
+                $("#彈跳框2").dialog("open");
+
+            });
+
+        });
+     $(function() {
+
+            $("#彈跳框3").dialog({
+
+                autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a3").click(function() {
+
+                $("#彈跳框3").dialog("open");
+
+            });
+
+        });
+     $(function() {
+
+            $("#彈跳框4").dialog({
+
+                autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a4").click(function() {
+
+                $("#彈跳框4").dialog("open");
+
+            });
+
+        });
+     $(function() {
+
+            $("#彈跳框5").dialog({
+
+                autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a5").click(function() {
+
+                $("#彈跳框5").dialog("open");
+
+            });
+
+        });
+     $(function() {
+
+            $("#彈跳框6").dialog({
+
+                autoOpen: false,width:600,height:500,modal: true,
+                buttons:{
+                    "確定": function() { $(this).dialog("close"); }
+                }
+
+            });
+
+            $("#a6").click(function() {
+
+                $("#彈跳框6").dialog("open");
+
+            });
+
+        });
